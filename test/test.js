4a1b7fe2a33aba41d9c07085fe5cfc3f46544a3a
Santosh Rajan <santrajan@gmail.com>
2013-07-07 15:48:43 +0530

    Version 0.0.7

---
+++ test/test.js
@@ -0,0 +1,16 @@
+var test = require("tape")
+    $    = require("../tinix")
+
+test("Compatibility Test", function(t) {
+    t.plan(10)
+    t.equal(typeof XMLHttpRequest, 'function')
+    t.equal(typeof JSON, 'object')
+    t.equal(typeof document.readyState, 'string')
+    t.equal(typeof document.onreadystatechange, 'object')
+    t.equal(typeof document.cookie, 'string')
+    t.equal(typeof document.addEventListener, 'function')
+    t.equal(typeof document.querySelector, 'function')
+    t.equal(typeof document.querySelectorAll, 'function')
+    t.equal(typeof Array.prototype.forEach, 'function')
+    t.equal(typeof Array.prototype.map, 'function')
+})
