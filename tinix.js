6f7819f8ebfae15ff46179718be84090e54b35ae
aksanoble <aksanoble@gmail.com>
2015-05-09 01:50:46 +0530

    remove functional stuff and cookies support

--- tinix.js
+++ tinix.js
@@ -3,7 +3,6 @@
 // Copyright 2013 - Santosh Rajan - santoshrajan.com
 //
 
-
 if (!Element.prototype.on) Element.prototype.on = Element.prototype.addEventListener
 
 var tinix = function(id) {
@@ -83,81 +82,5 @@ tinix.postJSON = function(u, b, c) {
     this.post(u, JSON.stringify(b), "application/json", c)
 }
 
-// Functional Stuff
-
-tinix.curry = function(fn, numArgs) {
-    numArgs = numArgs || fn.length
-    return function f(largs) {
-        return function() {
-            var args = Array.prototype.concat.apply(largs, arguments)
-            return args.length === numArgs ? fn.apply(null, args) : f(args)
-        }
-    }([])
-}
-
-tinix.compose = function() {
-    var funcs = Array.prototype.slice.call(arguments)
-    return function(x) {
-        return funcs.reduceRight(function(prev_val, next) {
-            return next(prev_val)
-        }, x)
-    }
-}
-
-
-/*\
-|*|
-|*|  :: cookies.js ::
-|*|
-|*|  A complete cookies reader/writer framework with full unicode support.
-|*|
-|*|  https://developer.mozilla.org/en-US/docs/DOM/document.cookie
-|*|
-|*|  This framework is released under the GNU Public License, version 3 or later.
-|*|  http://www.gnu.org/licenses/gpl-3.0-standalone.html
-|*|
-|*|  Syntaxes:
-|*|
-|*|  * docCookies.setItem(name, value[, end[, path[, domain[, secure]]]])
-|*|  * docCookies.getItem(name)
-|*|  * docCookies.removeItem(name[, path])
-|*|  * docCookies.hasItem(name)
-|*|  * docCookies.keys()
-|*|
-\*/
- 
-tinix.cookies = {
-  getItem: function (sKey) {
-    return unescape(document.cookie.replace(new RegExp("(?:(?:^|.*;\\s*)" + escape(sKey).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=\\s*((?:[^;](?!;))*[^;]?).*)|.*"), "$1")) || null;
-  },
-  setItem: function (sKey, sValue, vEnd, sPath, sDomain, bSecure) {
-    if (!sKey || /^(?:expires|max\-age|path|domain|secure)$/i.test(sKey)) { return false; }
-    var sExpires = "";
-    if (vEnd) {
-      switch (vEnd.constructor) {
-        case Number:
-          sExpires = vEnd === Infinity ? "; expires=Fri, 31 Dec 9999 23:59:59 GMT" : "; max-age=" + vEnd;
-          break;
-        case String:
-          sExpires = "; expires=" + vEnd;
-          break;
-        case Date:
-          sExpires = "; expires=" + vEnd.toGMTString();
-          break;
-      }
-    }
-    document.cookie = escape(sKey) + "=" + escape(sValue) + sExpires + (sDomain ? "; domain=" + sDomain : "") + (sPath ? "; path=" + sPath : "") + (bSecure ? "; secure" : "");
-    return true;
-  },
-  removeItem: function (sKey, sPath) {
-    if (!sKey || !this.hasItem(sKey)) { return false; }
-    document.cookie = escape(sKey) + "=; expires=Thu, 01 Jan 1970 00:00:00 GMT" + (sPath ? "; path=" + sPath : "");
-    return true;
-  },
-  hasItem: function (sKey) {
-    return (new RegExp("(?:^|;\\s*)" + escape(sKey).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=")).test(document.cookie);
-  }
-};
 
 module.exports = tinix
-
